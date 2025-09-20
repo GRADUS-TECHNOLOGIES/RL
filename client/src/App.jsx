6c9e5554f4a2d4bf8f8e180003ad050c65c53507
Jelp200 <jorge.pena0516@gmail.com>
2025-09-20 16:23:51 -0600

    :file_folder: [MOD] ARCHIVOS RL

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,52 +1,15 @@
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
 
 //* IMPORT DE PAGINAS
-import Home from './pages/Home.jsx'
-import About from './pages/About.jsx'
-import SignIn from './pages/SignIn.jsx'
-import SignUp from './pages/SignUp.jsx'
-import Dashboard from './pages/Dashboard.jsx'
-import Projects from './pages/Projects.jsx'
-import Purpose from './pages/Purpose.jsx'
-import ErrorPage from './pages/ErrorPage.jsx'
-import UnderConstructionPage from './pages/UnderConstructionPage.jsx'
-
-//* IMPORT DE COMPONENTES
-import Header from './components/Header.jsx'
-import Footer from './components/Footer.jsx'
-import PrivateRoute from './components/PrivateRoute.jsx'
-import OnlyAdminPrivateRoute from './components/OnlyAdminPrivateRoute.jsx'
-import CreatePost from './pages/CreatePost.jsx'
-import UpdatePost from './pages/UpdatePost.jsx'
-import PostPage from './pages/PostPage.jsx'
-import ScrollToTop from './components/ScrollToTop.jsx'
-import Search from './pages/Search.jsx'
+import UnderConstructionPage2 from './pages/UnderConstPage.jsx'
 
 export default function App() {
   return (
     <Router>
-      <ScrollToTop />
-      <Header />
       <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/about" element={<About />} />
-        <Route path="/sign-in" element={<SignIn />} />
-        <Route path="/sign-up" element={<SignUp />} />
-        <Route path="/search" element={<Search />} />
-        <Route element={<PrivateRoute />}>
-          <Route path="/dashboard" element={<Dashboard />} />
-        </Route>
-        <Route element={<OnlyAdminPrivateRoute />}>
-          <Route path="/create-post" element={<CreatePost />} />
-          <Route path="/update-post/:postId" element={<UpdatePost />} />
-        </Route>
-        <Route path="/services" element={<Projects />} />
-        <Route path="/post/:postSlug" element={<PostPage />} />
-        <Route path="/purpose" element={<Purpose />} />
-        <Route path="/error" element={<ErrorPage />} />
-        <Route path="/construccion" element={<UnderConstructionPage />} />
+        <Route path="/" element={<UnderConstructionPage2 />} />
       </Routes>
-      <Footer />
     </Router>
   )
 }
+
---
+++ client/src/pages/UnderConstPage.jsx
@@ -0,0 +1,10 @@
+// src/pages/UnderConstructionPage.jsx
+export default function UnderConstructionPage() {
+    return (
+        <div className="flex flex-col items-center justify-center h-screen text-center bg-gray-50">
+            <h1 className="text-4xl md:text-6xl font-bold text-gray-800 mb-4">
+                🚧 :(
+            </h1>
+        </div>
+    )
+}
