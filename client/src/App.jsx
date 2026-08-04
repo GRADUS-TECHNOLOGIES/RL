f26a2ea2d61717d5e7d7cc2ad11fbba75150a7e0
Jelp200 <jorge.pena0516@gmail.com>
2026-08-03 20:28:27 -0600

    :zap: [MOD] Blog deactivate

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -25,7 +25,7 @@ import Search from './pages/Search.jsx'
 
 //* CONTROL DE DISPONIBILIDAD DEL SERVICIO
 //* Cambiar a `false` para reactivar el sitio
-const SERVICE_DISABLED = false
+const SERVICE_DISABLED = true
 
 export default function App() {
   if (SERVICE_DISABLED) {
